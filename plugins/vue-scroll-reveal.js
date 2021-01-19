e960073d219e3431fcf7914100802ac2e6f6df4a
Andreas Elia <andreaselia@live.co.uk>
2021-01-19 19:25:43 +0000

    remove class

--- plugins/vue-scroll-reveal.js
+++ plugins/vue-scroll-reveal.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueScrollReveal from 'vue-scroll-reveal'
 
 Vue.use(VueScrollReveal, {
-  class: 'v-scroll-reveal',
   delay: 375,
   duration: 300,
   distance: '60px',
