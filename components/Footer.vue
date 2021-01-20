2fc9bbaa471be352e8be603687726f2d597e2f57
Andreas Elia <andreaselia@live.co.uk>
2021-01-20 15:41:09 +0000

    footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,14 +7,14 @@
       <div class="grid gap-16 md:grid-cols-2">
         <div class="footer-half">
           <h3 class="mb-8">Contact</h3>
-          <p class="mb-2 text-2xl">hi@isnotanumber.com</p>
-          <p class="text-2xl">+44 20 7378 6699</p>
+          <a href="mailto:hi@isnotanumber.com" class="mb-2 text-2xl">hi@isnotanumber.com</a>
+          <a href="tel:+442073786699" class="text-2xl">+44 20 7378 6699</a>
         </div>
 
         <div class="footer-half">
           <h3 class="mb-8">Connect</h3>
-          <p class="mb-2 text-2xl">Linkedin</p>
-          <p class="text-2xl">Instagram</p>
+          <a href="#" class="mb-2 text-2xl">Linkedin</a>
+          <a href="#" class="text-2xl">Instagram</a>
         </div>
       </div>
     </div>
