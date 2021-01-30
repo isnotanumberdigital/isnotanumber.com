cc54ed04d41298808abffef2383b7700619b0421
AndreasElia <andreaselia@live.co.uk>
2021-01-30 23:29:11 +0000

    reveal options

--- plugins/vue-scroll-reveal.js
+++ plugins/vue-scroll-reveal.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueScrollReveal from 'vue-scroll-reveal'
 
 Vue.use(VueScrollReveal, {
-  delay: 375,
+  // delay: 375,
   duration: 300,
   distance: '60px',
   interval: 300,
