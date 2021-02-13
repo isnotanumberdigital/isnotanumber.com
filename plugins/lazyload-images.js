c9d64675908d26b87ca3e6b9d734401413d2f8de
AndreasElia <andreaselia@live.co.uk>
2021-02-13 15:04:51 +0000

    indent fix

--- plugins/lazyload-images.js
+++ plugins/lazyload-images.js
@@ -1,7 +1,9 @@
-import 'lazysizes';
-document.addEventListener('lazybeforeunveil', function(e){
-    var bg = e.target.getAttribute('data-bg');
-    if(bg){
-        e.target.style.backgroundImage = 'url(' + bg + ')';
-    }
-});
+import 'lazysizes'
+
+document.addEventListener('lazybeforeunveil', function(e) {
+  const bg = e.target.getAttribute('data-bg')
+
+  if (bg) {
+    e.target.style.backgroundImage = 'url(' + bg + ')'
+  }
+})
